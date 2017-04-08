4a95e86b87722a26e2859723faeb8c1eec0f25ee
Sean Worthington <sean@worthington.net>
2017-04-08 08:21:16 -0700

    Will be needed if Newtonsoft.Json is used

---
+++ StringHolderTemp.cs
@@ -0,0 +1,150 @@
+﻿
+
+namespace Foundation
+{
+    public class StringHolderTemp
+    {
+
+        public string program_command_1 { get; set; }
+        public string program_command_2 { get; set; }
+        public  string program_command_3 { get; set; }
+        public  string program_command_4 { get; set; }
+        public  string program_command_5 { get; set; }
+        public  string program_command_6 { get; set; }
+        public  string program_command_7 { get; set; }
+        public  string program_command_8 { get; set; }
+        public  string program_run_1 { get; set; }
+        public  string program_run_2 { get; set; }
+
+        public  string program_start_1 { get; set; }
+        public  string program_start_2 { get; set; }
+        public  string program_start_3 { get; set; }
+        public  string program_start_4 { get; set; }
+        public  string program_start_5 { get; set; }
+        public  string program_start_6 { get; set; }
+        public  string program_echo_1 { get; set; }
+        public  string program_echo_2 { get; set; }
+        public  string program_echo_3 { get; set; }
+        public  string program_echo_4 { get; set; }
+        public  string program_echo_5 { get; set; }
+        public  string program_echo_6 { get; set; }
+        public  string raida0 { get; set; }
+        public  string raida1 { get; set; }
+        public  string raida2 { get; set; }
+        public  string raida3 { get; set; }
+        public  string raida4 { get; set; }
+        public  string raida5 { get; set; }
+        public  string raida6 { get; set; }
+        public  string raida7 { get; set; }
+        public  string raida8 { get; set; }
+        public  string raida9 { get; set; }
+        public  string raida10 { get; set; }
+        public  string raida11 { get; set; }
+        public  string raida12 { get; set; }
+        public  string raida13 { get; set; }
+        public  string raida14 { get; set; }
+        public  string raida15 { get; set; }
+        public  string raida16 { get; set; }
+        public  string raida17 { get; set; }
+        public  string raida18 { get; set; }
+        public  string raida19 { get; set; }
+        public  string raida20 { get; set; }
+        public  string raida21 { get; set; }
+        public  string raida22 { get; set; }
+        public  string raida23 { get; set; }
+        public  string raida24 { get; set; }
+        public  string program_showcoins_total { get; set; }
+        public  string program_showcoins_perfect { get; set; }
+        public  string program_showcoins_fracked { get; set; }
+        public  string program_showFolders_1 { get; set; }
+        public  string program_showFolders_2 { get; set; }
+        public  string program_showFolders_3 { get; set; }
+        public  string program_showFolders_4 { get; set; }
+        public  string program_showFolders_5 { get; set; }
+        public  string program_showFolders_6 { get; set; }
+        public  string program_showFolders_7 { get; set; }
+        public  string program_showFolders_8 { get; set; }
+        public  string program_showFolders_9 { get; set; }
+        public  string program_showFolders_10 { get; set; }
+        public  string program_showFolders_11 { get; set; }
+        public  string program_import_1 { get; set; }
+        public  string program_import_2 { get; set; }
+        public  string program_import_3 { get; set; }
+        public  string program_import_4 { get; set; }
+
+        public  string program_detect_1 { get; set; }
+        public  string program_detect_2 { get; set; }
+        public  string program_detect_3 { get; set; }
+        public  string program_detect_4 { get; set; }
+        public  string program_detect_5 { get; set; }
+
+        public  string program_dump_1 { get; set; }
+        public  string program_dump_2 { get; set; }
+        public  string program_dump_3 { get; set; }
+        public  string program_dump_4 { get; set; }
+        //Note this code is used in export and export for sales
+        public  string program_dump_5 { get; set; }
+        public  string program_dump_6 { get; set; }
+        public  string program_dump_7 { get; set; }
+        public  string program_dump_8 { get; set; }
+        public  string program_dump_9 { get; set; }
+        public  string program_dump_10 { get; set; }
+        public  string program_dump_11 { get; set; }
+
+        public  string program_export_1 { get; set; }
+        public  string program_export_2 { get; set; }
+        public  string program_export_3 { get; set; }
+
+        public  string program_fix_1 { get; set; }
+        public  string program_fix_2 { get; set; }
+        public  string program_fix_3 { get; set; }
+        public  string program_fix_4 { get; set; }
+        public  string program_fix_5 { get; set; }
+
+
+        public  string cloudcoin_report { get; set; }
+        public  string cloudcoin_denomination { get; set; }
+
+        public  string detector_3 { get; set; }
+        public  string detector_4 { get; set; }
+        public  string detector_5 { get; set; }
+        public  string detector_6 { get; set; }
+        public  string detector_7 { get; set; }
+        public  string detector_8 { get; set; }
+
+        public  string frackfixer_1 { get; set; }
+        public  string frackfixer_2 { get; set; }
+        public  string frackfixer_3 { get; set; }
+        public  string frackfixer_4 { get; set; }
+        public  string frackfixer_5 { get; set; }
+        public  string frackfixer_6 { get; set; }
+        public  string frackfixer_unfracking { get; set; }
+        public  string frackfixer_8 { get; set; }
+        public  string frackfixer_9 { get; set; }
+        public  string frackfixer_10 { get; set; }
+        public  string frackfixer_11 { get; set; }
+        public  string frackfixer_12 { get; set; }
+        public  string frackfixer_13 { get; set; }
+
+        public  string importer_1 { get; set; }
+        public  string importer_2 { get; set; }
+        public  string importer_3 { get; set; }
+        public  string importer_importstack1 { get; set; }
+        public  string importer_importstack2 { get; set; }
+
+        public  string importer_seemsValidJSON_1 { get; set; }
+        public  string importer_seemsValidJSON_2 { get; set; }
+        public  string importer_seemsValidJSON_3 { get; set; }
+        public  string importer_seemsValidJSON_4 { get; set; }
+
+        public  string keyboardreader_1 { get; set; }
+        public  string keyboardreader_2 { get; set; }
+        public  string keyboardreader_3 { get; set; }
+        public  string keyboardreader_4 { get; set; }
+        public  string keyboardreader_5 { get; set; }
+        public  string keyboardreader_6 { get; set; }
+        public  string keyboardreader_7 { get; set; }
+
+
+    }
+}
