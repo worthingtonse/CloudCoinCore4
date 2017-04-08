2c8dd3f8a8c9d4d73443e50accd9acb1f2884fb5
Sean Worthington <sean@worthington.net>
2017-04-08 08:20:28 -0700

    Will be needed if Newtonsoft.Json is used

---
+++ SimpleCoin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foundation
+{
+    class SimpleCoin
+    {
+        public int sn { get; set; }
+        public int nn { get; set; }
+        public List<string> an { get; set; }
+        public string ed { get; set; }
+        public List<string> aoid { get; set; }
+    }
+}
